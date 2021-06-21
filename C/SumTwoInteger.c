6d4eb180bd01477c1cf3894cfa5241319ef2d467
onlitstechnologies <onlitstechnologies@gmail.com>
2021-06-21 12:28:20 +0530

    20210618

---
+++ C/SumTwoInteger.c
@@ -0,0 +1,17 @@
+#include<stdio.h>	//preprocessor directive (include header file)
+int main()			//entry point of the program (comments/remarks)
+{
+	int a, b, c;
+	printf("Enter the first number: ");
+	scanf("%d", &a);
+	printf("Enter the second number: ");
+	scanf("%d", &b);
+	c = a + b;
+	printf("The sum is %d.\n", c);
+	return 0;
+}
+
+/*
+	32 Keyword - dictionary/reserved word
+	ANSI C	-	
+*/
