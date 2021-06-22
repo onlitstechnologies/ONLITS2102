8d58123fdd8be273b9d851dfe2c7d3b9f8a8ff96
onlitstechnologies <onlitstechnologies@gmail.com>
2021-06-23 05:19:20 +0530

    20210622

---
+++ C/ANSI_C/CH01/1.1/prog.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("Manish Kumar Mishra\n");
+	printf("11/156, Srinagar Colony\n");
+	printf("Muzaffarpur, 842001\n");
+	return 0;
+}
---
+++ C/ANSI_C/CH01/1.5/prog.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+	float r=5, a;
+	a = PI * r * r;
+	printf("The area of circle is %1.2f sq. units.\n", a);
+	return 0;
+}
